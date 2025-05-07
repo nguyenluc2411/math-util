13c3bb027cfac7c4dcea8a1c95234a7e7bdf174a
nguyenluc2411 <lucn0702@gmail.com>
2025-05-07 17:58:06 +0700

    refactor the main code

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 ### You will find in this repo the following stuff:
 * The Math-Util library that offers multiple math function (a Java-based source code)
 * Unit Testing source code with JUnit framework
+* GitHub Actions to demonstrate the Continuous Integration (CI) technique
+* Regression Test
 * ...
+
+### Screenshots
+![DDT & TDD with JUnit](https://github.com/nguyenluc2411/math-util/blob/main/images/DDT%20with%20JUnit.png)
 
 #### © 2025 lucnd
--- src/com/lucnd/mathutil/core/MathUtil.java
+++ src/com/lucnd/mathutil/core/MathUtil.java
@@ -10,7 +10,22 @@ package com.lucnd.mathutil.core;
  */
 public class MathUtil {
 
-    public static long getFactorial(int n) {
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//        }
+//
+//        if (n == 0 || n == 1 ) {
+//            return 1;
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n ; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+     public static long getFactorial(int n) {
 
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
@@ -19,10 +34,6 @@ public class MathUtil {
         if (n == 0 || n == 1 ) {
             return 1;
         }
-        long result = 1;
-        for (int i = 2; i <= n ; i++) {
-            result *= i;
-        }
-        return result;
+        return n * getFactorial(n - 1);
     }
 }
